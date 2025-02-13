9dd38b4828906e951429c626437524b9317cdd56
Matias Courroux <mcourroux@hotmail.com>
2025-02-13 11:57:05 -0300

    conexion a la db

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,18 @@
 import express from 'express' 
 import colors from 'colors'
 import morgan from 'morgan'
+import { db } from './config/db'
+
+async function conectDB() {
+    try {
+        await db.authenticate();
+        db.sync();
+        console.log(colors.blue.bold('Conexión exitosa a la DB'));
+    } catch (error) {
+        console.log(colors.red.bold('Fallo la conexión a la DB'));
+    }
+}
+conectDB();
 
 const app = express()
 
